a50cbfb00440ddd2b5db257f95cf9b9ac2f04c78
Chaow Porkaew <chaow.po@up.ac.th>
2018-07-04 21:53:00 +0700

    rm package-lock.json

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    API_URL : '"http://localhost:10001"',
+    // API_URL : '"http://localhost:10001"',
+    API_URL : '"http://pigfarm.miix.local"',
 });
