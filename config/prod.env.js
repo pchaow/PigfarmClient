13260e56c753db03965770c50fa39792306b77bd
Chaow Porkaew <chaow.po@up.ac.th>
2019-04-02 13:03:14 +0700

    change main server to mct server

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
     NODE_ENV: '"production"',
-    API_URL : '"https://ec2-13-250-40-240.ap-southeast-1.compute.amazonaws.com"',
+    // API_URL : '"https://ec2-13-250-40-240.ap-southeast-1.compute.amazonaws.com"',
+API_URL : '"http://mct.ict.up.ac.th:10005"',
 
 };
